f9b83b15383ff3a7abe488452513542c4ed8eea3
LazaroQueiroz <lazaro.queiroz.nascimento@ccc.ufcg.edu.br>
2025-06-05 17:53:04 -0300

    feat: adding automation, pipeline to creating a result csv

--- simulator_scrapper.py
+++ simulator_scrapper.py
@@ -4,7 +4,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
+import json
 from dotenv import load_dotenv, dotenv_values
+
+
+def formatLatency(a: str) -> str:
+    n_s = "".join(list(filter(lambda x: x.isdigit() or x == ".", div.text)))
+    f = float(n_s)
+    s = f"{f:.2f}"
+    return s
+
 
 load_dotenv()
 
@@ -14,12 +23,22 @@ CLASSE = os.getenv("CLASS")
 driver = webdriver.Firefox()
 driver.get(URL)
 
-wait = WebDriverWait(driver, 10)  # Espera até 10 segundos
-print("latency")
+
+data = {}
+data_file = open("config.json", "r")
+data = json.load(data_file)
+
+result_file = open(data["result_path"], "w")
+
+wait = WebDriverWait(driver, 15)  # Espera até 10 segundos
+result_file.write("latency\n")
+
 try:
     while True:
         div = wait.until(EC.presence_of_element_located((By.CLASS_NAME, CLASSE)))
-        print(f"Valor da div: {div.text}")
-        time.sleep()  # 500 ms
+        result_file.write(f"{formatLatency(div.text)}\n")
+        time.sleep(1)  # 500 ms
 except KeyboardInterrupt:
     driver.quit()
+    result_file.close()
+    data_file.close()
